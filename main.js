849e56747441e202551d58b5b5c59648d150ba93
Mauiricio Luna <shelldon.luna@gmail.com>
2022-07-30 14:30:37 -0500

    fin de la seccion de funciones js

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
+const render = () => {
+    const todoList = document.getElementById('todo-list')
+    const todosTemplate = todos.map(t=> '<li>'+ t + '</li>');
+    todoList.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li')
+    elementos.forEach((elemento, i) => {
+        elemento.addEventListener('click', () => {
+            elemento.parentNode.removeChild(elemento)
+            todos.splice(i,1)
+            actualizaTodos(todos);
+            render()
+        })
+    })
+}
+
+const actualizaTodos = (todos) => {
+    const todosStrings = JSON.stringify(todos)
+    localStorage.setItem('todos',todosStrings)
+}
 window.onload = () => {
+    render()
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
         e.preventDefault();
         const todo = document.getElementById('todo');
         const todoText = todo.value;
         todo.value = '';
-        console.log (todoText);
         todos.push(todoText);
-        const todoList = document.getElementById('todo-list')
-        const todosTemplate = todos.map(t=> '<li>'+ t + '</li>');
-        todoList.innerHTML = todosTemplate.join('');
-        const elementos = document.querySelectorAll('#todo-list li')
-        elementos.forEach((elemento, i) => {
-            elemento.addEventListener('click', () => {
-                elemento.parentNode.removeChild(elemento)
-                todos.splice(i,1)
-
-            })
-        })
-            
+        actualizaTodos(todos);
+        render()
         }
 
     }
