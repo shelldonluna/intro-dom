dd13b755dc1969293835a1c9a01141b875c0cfa0
Mauricio Luna <shelldon.luna@gmail.com>
2022-07-29 22:22:09 -0500

    113. Eliminando elementos

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+const todos = [];
+
 window.onload = () => {
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
-        e.preventDefault;
+        e.preventDefault();
         const todo = document.getElementById('todo');
         const todoText = todo.value;
         todo.value = '';
-        console.log (todoText)
+        console.log (todoText);
+        todos.push(todoText);
+        const todoList = document.getElementById('todo-list')
+        const todosTemplate = todos.map(t=> '<li>'+ t + '</li>');
+        todoList.innerHTML = todosTemplate.join('');
+        const elementos = document.querySelectorAll('#todo-list li')
+        elementos.forEach((elemento, i) => {
+            elemento.addEventListener('click', () => {
+                elemento.parentNode.removeChild(elemento)
+                todos.splice(i,1)
+
+            })
+        })
+            
+        }
+
     }
-}
+
